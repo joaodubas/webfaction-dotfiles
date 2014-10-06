cc9ea7057f0fba7590c2299f429f11e4fdc2ee89
João Paulo Dubas <joao.dubas@gmail.com>
2014-10-06 15:12:03 -0300

    [update] docker is available under https schema.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -43,7 +43,7 @@ function upgrade_system() {
 	# ppa for vim
 	add-apt-repository -y ppa:fcwu-tw/ppa
 	wget -qO- https://get.docker.io/gpg | apt-key add -
-	echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+	echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
 	apt-get update -y --quiet
 
 	echo "install needed deps"
